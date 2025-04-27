f7c4db9eba1095403b9fa3bd6064aa3b33626798
Yulin Zhu <zieft@hotmail.com>
2025-04-27 14:50:11 +0200

    Route Convention： 1. 所有route都要放在app文件夹内，2. Route文件必须命名为page.jsx （或page.js, page.tsx） 3. 每个文件夹代表URL Path的一部分

---
+++ hello-world/src/app/page.jsx
@@ -0,0 +1,10 @@
+/*
+    Route Convention：
+        1. 所有route都要放在app文件夹内，
+        2. Route文件必须命名为page.jsx （或page.js, page.tsx） 
+        3. 每个文件夹代表URL Path的一部分 
+*/
+
+export default function Home() {
+    return <h1>Welcome Home</h1>;
+}
