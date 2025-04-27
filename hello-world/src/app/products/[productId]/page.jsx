b211e6674f1f847f60d375cc6656419accba8403
Yulin Zhu <zieft@hotmail.com>
2025-04-27 15:46:28 +0200

    Dynamic Routes part 1

---
+++ hello-world/src/app/products/[productId]/page.jsx
@@ -0,0 +1,10 @@
+export default function ProductDetail(){
+    return <h1>Details about product</h1>
+}
+
+
+/*
+    [productId]起作用的部分是方括号
+    实际在访问url时，localhost:3000/products/后面跟任何字符都能访问到这个page
+    下一个commit里讲解如何拿到用户传入的url并动态地展示在页面上
+*/
---
+++ hello-world/src/app/products/page.jsx
@@ -0,0 +1,9 @@
+export default function ProductList(){
+    return (
+    <>
+        <h1>Product list</h1>
+        <h2>Peoduct 1</h2>
+        <h2>Peoduct 2</h2>
+        <h2>Peoduct 3</h2>
+    </>)
+}
