51d1b66aa1f719ae9397b3793beb3ad3c46f4f9c
Yulin Zhu <zieft@hotmail.com>
2025-04-28 15:35:11 +0200

    case 6 catch-all segment  part 1

---
+++ hello-world/src/app/docs/[...slug]/page.jsx
@@ -0,0 +1,10 @@
+export default function Docs() {
+    return <h1>Docs home page</h1>;
+}
+
+/*
+    src/app/docs/[...文件夹名]
+    这样的命名方式将匹配所有http://localhost:3000/docs/下的子url
+    但不包括http://localhost:3000/docs/本身
+*/
+
