6b8fed39fab2f943ebca82274d8afc1d03a3a3de
zieft <zieft@example.com>
2025-05-02 20:14:50 +0200

    Link component navigation

--- hello-world/src/app/products/page.jsx
+++ hello-world/src/app/products/page.jsx
@@ -1,9 +1,20 @@
+import Link from "next/link"
+
 export default function ProductList(){
+    const productId = 100
     return (
     <>
         <h1>Product list</h1>
-        <h2>Peoduct 1</h2>
+        <h2>
+            <Link href="/products/1">Peoduct 1</Link>
+        </h2>
         <h2>Peoduct 2</h2>
-        <h2>Peoduct 3</h2>
+
+        <h2>
+            <Link href="/products/3" replace>Peoduct 3</Link>
+        </h2>
+        {/* replace的作用就是在用户点击连接跳转到新页面后，按返回键会直接返回到主页 */}
+        <Link href={`/products/${productId}`}>Product {productId}</Link>
+        
     </>)
 }
