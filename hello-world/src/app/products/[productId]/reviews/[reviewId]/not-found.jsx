0c3aee7caccaf618eb7a1af6bb696197abebcbe1
Yulin Zhu <zieft@hotmail.com>
2025-05-02 11:18:00 +0200

    Not found page part 3 在404页面中解析URL中的参数

--- hello-world/src/app/products/[productId]/reviews/[reviewId]/not-found.jsx
+++ hello-world/src/app/products/[productId]/reviews/[reviewId]/not-found.jsx
@@ -1,7 +1,25 @@
-export default function NotFound(){
+"use client"
+
+/*
+    usePathname() 只能用在client component中， 
+    'use client'可强行将服务器组件变成客户端组件
+*/
+
+
+import { usePathname } from "next/navigation"
+/*
+    由于NotFound函数不能接受params，
+    因此需要用到usePathname来解析url中的参数
+*/
+
+export default function NotFound() {
+    const pathname = usePathname()
+    const productId = pathname.split("/")[2]
+    const reviewId = pathname.split("/")[4]
+
     return (
         <div>
-            <h2>Review not found</h2>
+            <h2>Review {reviewId} not found for product {productId}</h2>
         </div>
     )
 }
