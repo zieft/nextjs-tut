3078a785a41ebfc0a88f52d7ec556f8cc31bb492
Yulin Zhu <zieft@hotmail.com>
2025-05-02 11:06:40 +0200

    Not found page part 2 给子目录添加单独的404页面

--- hello-world/src/app/products/[productId]/reviews/[reviewId]/page.jsx
+++ hello-world/src/app/products/[productId]/reviews/[reviewId]/page.jsx
@@ -1,6 +1,13 @@
+import { notFound } from "next/navigation";
+
 export default async function ReviewDetail({ params }) {
-    
+
     const { productId, reviewId } = await params
+
+    if (parseInt(reviewId) > 1000) {
+        notFound();
+
+    }
 
     return <h1>Review {reviewId} for product {productId}.</h1>
 }
