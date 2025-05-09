89d7fd46a0b4a0aac93cfef22278583baa39e356
Yulin Zhu <zieft@hotmail.com>
2025-05-09 12:21:13 +0200

    redirect examples

---
+++ hello-world/src/app/order-product/page.jsx
@@ -0,0 +1,17 @@
+"use client"
+import {useRouter} from "next/navigation";
+
+export default function OrderProduct() {
+    const router = useRouter()
+    const handleClick = () => {
+        console.log('placing your order');
+        router.push("/")
+    }
+
+    return (
+        <>
+            <h1>Order Product</h1>
+            <button onClick={handleClick}>Place Order</button>
+        </>
+    )
+}
--- hello-world/src/app/products/[productId]/reviews/[reviewId]/page.jsx
+++ hello-world/src/app/products/[productId]/reviews/[reviewId]/page.jsx
@@ -1,12 +1,12 @@
-import { notFound } from "next/navigation";
+import {notFound, redirect} from "next/navigation";
 
-export default async function ReviewDetail({ params }) {
+export default async function ReviewDetail({params}) {
 
-    const { productId, reviewId } = await params
+    const {productId, reviewId} = await params
 
     if (parseInt(reviewId) > 1000) {
-        notFound();
-
+        // notFound();
+        redirect("/products")
     }
 
     return <h1>Review {reviewId} for product {productId}.</h1>
