57ccb5923be97916cfef8c7b79c793f336ed9a4d
Yulin Zhu <zieft@hotmail.com>
2025-05-10 14:26:18 +0200

    error.jsx在目录中的位置直接影响哪些组件将被error.jsx内定义的组件替换，而layout.jsx中的异常不能被error.jsx处理

--- hello-world/src/app/products/[productId]/layout.jsx
+++ hello-world/src/app/products/[productId]/layout.jsx
@@ -1,4 +1,14 @@
+function getRandomInt(count) {
+    return Math.floor(Math.random() * count)
+}
+
+
 export default function ProductDetailLayout({ children }) {
+    //随机抛出错误
+    const random=getRandomInt(2)
+    if (random === 1) {
+        throw new Error("Error loading product")
+    }
     return (
         <>
             {children}
